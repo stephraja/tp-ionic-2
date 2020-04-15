15c6a89e035d8ed7b9a20873794096b27c3bdc30
rajaynov <rajaoarisoa.ranaivosoa@ynov.com>
2020-04-15 21:03:38 +0200

    Create getLocation function wich use geolocation ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+type Position = {
+  latitude: number
+  longitude: number
+}
+
 export class HomePage {
 
-  title: string
-  constructor() {}
+  title: string;
+  position: Position
+  constructor(private geolocation: Geolocation) {}
 
   public updateTitle(newTitle: string) {
     this.title = newTitle
+  }
+
+  async getPosition() {
+    await this.geolocation.getCurrentPosition().then((result: Geoposition) => {
+      this.position.latitude = result.coords.latitude
+      this.position.longitude = result.coords.longitude
+    })
   }
 
 }
