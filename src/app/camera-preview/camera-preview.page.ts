6afb2be59e052477b402f670120dd0bb6e7e6b26
rajaynov <rajaoarisoa.ranaivosoa@ynov.com>
2020-04-16 12:27:17 +0200

    Add camera preview on page

--- src/app/camera-preview/camera-preview.page.ts
+++ src/app/camera-preview/camera-preview.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview/ngx';
+import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview/ngx';
 
 
 @Component({
@@ -9,9 +9,21 @@ import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, Camer
 })
 export class CameraPreviewPage implements OnInit {
 
-  constructor() { }
+  constructor(private cameraPreview: CameraPreview) { }
 
   ngOnInit() {
+    const cameraPreviewOpts: CameraPreviewOptions = {
+      x: 0,
+      y: 0,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: 'back',
+      tapPhoto: true,
+      previewDrag: true,
+      alpha: 1
+    }
+
+    this.cameraPreview.startCamera(cameraPreviewOpts)
   }
 
 }
